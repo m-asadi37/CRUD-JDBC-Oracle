0da33c196819b3a99ba86ec729aa0bcb5f39e678
Mohammad Asadi <masd9617@gmail.com>
2024-09-19 19:21:17 +0330

    find all query

--- src/main/java/org/example/repository/impl/PersonRepoJdbcImpl.java
+++ src/main/java/org/example/repository/impl/PersonRepoJdbcImpl.java
@@ -2,7 +2,10 @@ package org.example.repository.impl;
 
 import org.example.entity.Person;
 import org.example.repository.PersonRepository;
+import org.example.repository.base.DBConnect;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonRepoJdbcImpl implements PersonRepository {
@@ -24,7 +27,31 @@ public class PersonRepoJdbcImpl implements PersonRepository {
 
     @Override
     public List<Person> findAll() {
-        return null;
+        List<Person> personList = new ArrayList<>();
+
+        try (DBConnect dbConnect = new DBConnect()) {
+            Connection con = dbConnect.getConnection();
+
+            PreparedStatement stm = con.prepareStatement("select * from PERSON");
+            ResultSet set = stm.executeQuery();
+            while (set.next()){
+                long id = set.getLong(1);
+                String name = set.getString(2);
+                String family = set.getString(3);
+                Date date = set.getDate(4);
+                String email = set.getString(5);
+                String phone = set.getString(6);
+                String address = set.getString(7);
+                Person p = new Person(id, name, family, date.toLocalDate(), email, phone, address);
+                personList.add(p);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return personList;
     }
 
     @Override
