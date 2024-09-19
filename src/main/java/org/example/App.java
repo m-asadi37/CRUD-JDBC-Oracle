582fd4ac6d2732c55fa95ec2327021864e90f126
Mohammad Asadi <masd9617@gmail.com>
2024-09-19 18:58:52 +0330

    abstraction on repository layer to have multiple implementations

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,11 +2,12 @@ package org.example;
 
 import org.example.controller.PersonController;
 import org.example.repository.PersonRepository;
+import org.example.repository.impl.PersonRepoJavaImpl;
 import org.example.service.PersonService;
 
 public class App {
     public static void main(String[] args) {
-        PersonRepository repository = new PersonRepository();
+        PersonRepository repository = new PersonRepoJavaImpl();
         PersonService service = new PersonService(repository);
         PersonController controller = new PersonController(service);
 
---
+++ src/main/java/org/example/repository/impl/PersonRepoJavaImpl.java
@@ -0,0 +1,62 @@
+package org.example.repository.impl;
+
+import org.example.entity.Person;
+import org.example.repository.PersonRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonRepoJavaImpl implements PersonRepository {
+    private List<Person> persons = new ArrayList<>();
+    private Long currentId = 1L;
+
+    public Person save(Person person) {
+        person.setId(currentId++);
+        persons.add(person);
+        return person;
+    }
+
+    public Person update(Long id, Person newPerson) {
+        for (Person person : persons) {
+            if (person.getId().equals(id)) {
+                person.setName(newPerson.getName());
+                person.setFamily(newPerson.getFamily());
+                person.setBirthDate(newPerson.getBirthDate());
+                person.setEmail(newPerson.getEmail());
+                person.setPhoneNumber(newPerson.getPhoneNumber());
+                person.setAddress(newPerson.getAddress());
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public boolean delete(Long id) {
+        return persons.removeIf(person -> person.getId().equals(id));
+    }
+
+    public List<Person> findAll() {
+        return new ArrayList<>(persons);
+    }
+
+    public Person findById(Long id) {
+        return persons.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Person findByNameAndFamily(String name, String family) {
+        return persons.stream()
+                .filter(person -> person.getName().equals(name) && person.getFamily().equals(family))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Person findByPhoneNum(String phoneNum) {
+        return persons.stream()
+                .filter(person -> person.getPhoneNumber().equals(phoneNum))
+                .findFirst()
+                .orElse(null);
+    }
+}
